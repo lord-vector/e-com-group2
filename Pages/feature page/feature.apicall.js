e28d528c84eb0c3b97316e95c86e01b7aab17dd3
lord-vector <r.anuj.agrawal@gmail.com>
2023-03-27 22:38:52 +0530

    Making changes to call the api based on category selected

--- Pages/feature page/feature.apicall.js
+++ Pages/feature page/feature.apicall.js
@@ -1,36 +1,37 @@
-const showItems = (items) => {
-  const ele = document.getElementsByClassName("grid");
-  str = "<ul>";
-  items.forEach((element) => {
-    str += `<div class="card" style="width: 18rem;">
-    <img src="${element.images[0]}" class="card-img-top" alt="..." />
-    <div class="card-body">
-      <h5 class="card-title">${element.title}</h5>
-      <p class="card-text">
-        ${element.description}
-      </p>
-      <a href="#" class="btn btn-primary">
-        Go somewhere
-      </a>
-    </div>
-  </div>`;
-  });
-  str += "</ul>";
-  const frag = document.createRange().createContextualFragment(str);
-  ele[0].appendChild(frag);
-  console.log(typeof frag);
-  console.log(ele[0]);
-};
+const queryString = window.location.search.split("=")[1];
 
-const getText = (category) => {
-  fetch(`https://dummyjson.com/products/category/${category}`)
+const getText = (requesedData) => {
+  fetch(`https://dummyjson.com/products/category/${requesedData}`)
     .then((res) => {
       console.log(res);
       res.json().then((data) => {
-        showItems(data.products);
         console.log(data.products);
-        // window.location.href = "./Pages/feature page/feature.html";
+        showItems(data.products);
       });
     })
     .catch((err) => console.log(err));
 };
+
+const showItems = (items) => {
+  const ele = document.getElementsByClassName("grid");
+  str = '<ul class="grid">';
+  items.forEach((element) => {
+    str += `<div class="card" style="width: 18rem;">
+      <img src="${element.images[0]}" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">${element.title}</h5>
+        <p class="card-text">
+          ${element.description}
+        </p>
+        <a href="#" class="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
+    </div>`;
+  });
+  str += "</ul>";
+  const frag = document.createRange().createContextualFragment(str);
+  ele[0].appendChild(frag);
+};
+
+getText(queryString);
