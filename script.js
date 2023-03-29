ecff5d6ea63fa3cd53532c90bc0d0e00e4029541
vishu pyasi <vpyasi11@gmail.com>
2023-03-29 21:38:45 +0530

    Most purchased section

--- script.js
+++ script.js
@@ -40,3 +40,58 @@ prevBtn.addEventListener('click', () => {
   prevCard();
   interval = setInterval(nextCard, 5000);
 });
+
+// Most Purchased section
+
+const prod = document.getElementById("product")
+/* <div class="card" style="width: 18rem;">
+      <img src="${items[count].images[0]}" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">${items[count].title}</h5>
+        <p class="card-text">
+          ${items[count].description}
+        </p>
+        <a href="#" class="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
+    </div> */
+
+
+    fetch(`https://dummyjson.com/products/`)
+    .then((res) => {
+      // console.log(res);
+      res.json().then((data) => {
+        console.log(data.products);
+        load(data.products);
+      });
+    })
+    .catch((err) => console.log(err));
+
+function load(data){
+  for(i=0;i<10;i++){
+    
+    var temp = Math.floor(Math.random()*30)
+
+    mPcard = document.createElement("div")
+    img = document.createElement("img")
+    img.src = data[temp].images[0]
+    mPcard.appendChild(img)
+
+    title = document.createElement("h5")
+    title.innerHTML = data[temp].title
+    description = document.createElement("p")
+    description.innerHTML = data[temp].description
+    btn = document.createElement("button")
+    btn.innerHTML = "Buy Now"
+
+    mPcard.appendChild(title)
+    mPcard.appendChild(description)
+    mPcard.appendChild(btn)
+    prod.appendChild(mPcard)
+  }
+  
+}
+
+
+
