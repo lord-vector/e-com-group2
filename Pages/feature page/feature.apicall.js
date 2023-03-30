02cd9d8fe33d7778d2def3abf584069fdaa26e65
vishu pyasi <vpyasi11@gmail.com>
2023-03-30 23:03:04 +0530

    Most purchased section

--- Pages/feature page/feature.apicall.js
+++ Pages/feature page/feature.apicall.js
@@ -1,7 +1,7 @@
 const queryString = window.location.search.split("=")[1];
 
-const getText = (requestedData) => {   
-  fetch(`https://dummyjson.com/products/category/${requestedData}`)
+const getText = (requesedData) => {
+  fetch(`https://dummyjson.com/products/category/${requesedData}`)
     .then((res) => {
       console.log(res);
       res.json().then((data) => {
@@ -11,56 +11,27 @@ const getText = (requestedData) => {
     })
     .catch((err) => console.log(err));
 };
-var count =0;
-const ele = document.getElementsByClassName("abc");
-const des = document.getElementById("des")
+
 const showItems = (items) => {
-  
-  
-  str = `<ul class="grid"><button onclick="back()"><</button>   
-     <div class="card" style="width: 18rem;">
-      <img src="${items[count].images[0]}" class="card-img-top" alt="..." />
+  const ele = document.getElementsByClassName("grid");
+  str = '<ul class="grid">';
+  items.forEach((element) => {
+    str += `<div class="card" style="width: 18rem;">
+      <img src="${element.images[0]}" class="card-img-top" alt="..." />
       <div class="card-body">
-        <h5 class="card-title">${items[count].title}</h5>
+        <h5 class="card-title">${element.title}</h5>
         <p class="card-text">
-          ${items[count].description}
+          ${element.description}
         </p>
         <a href="#" class="btn btn-primary">
           Go somewhere
         </a>
       </div>
-    </div>
-    <button onclick="forward()">></button></ul>`;
-  // });
-  // str += ;
-  const frag = document.createRange().createContextualFragment(str);  // why ?
+    </div>`;
+  });
+  str += "</ul>";
+  const frag = document.createRange().createContextualFragment(str);
   ele[0].appendChild(frag);
 };
 
 getText(queryString);
-
-function forward(){
-  if(count<4){
-    count +=1
-  }
-  else{
-    count = 0
-  }
-  ele[0].innerHTML = ""
-  getText(queryString);
- console.log(count)
- return count
-}
-function back(){
-  if(count>0){
-    count -=1
-  }
-  else{
-    count = 4
-  }
-  ele[0].innerHTML = ""
-  getText(queryString);
- console.log(count)
- return count
-}
-// console.log(count)  
