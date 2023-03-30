28718b8fb6e7392e0986d0ba577c3c28d4c75ccf
lord-vector <r.anuj.agrawal@gmail.com>
2023-03-30 23:19:05 +0530

    Css conditioning

--- Pages/feature page/feature.apicall.js
+++ Pages/feature page/feature.apicall.js
@@ -17,15 +17,24 @@ const showItems = (items) => {
   str = '<ul class="grid">';
   items.forEach((element) => {
     str += `<div class="card" style="width: 18rem;">
-      <img src="${element.images[0]}" class="card-img-top" alt="..." />
+      <img src="${
+        element.images[0]
+      }" class="card-img-top imageShadow" alt="..." />
       <div class="card-body">
         <h5 class="card-title">${element.title}</h5>
         <p class="card-text">
           ${element.description}
         </p>
-        <a href="#" class="btn btn-primary">
-          Go somewhere
-        </a>
+        <div class="actionButtons">
+            <div href="#" class="btn btn-primary ${(onclick = () => {
+              console.log(element.title);
+            })}">
+            Add to Cart
+            </div>
+            <div href="#" class="btn btn-primary">
+            Favourites
+            </div>
+        </div>
       </div>
     </div>`;
   });
