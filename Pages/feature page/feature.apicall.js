636bcbf9dcca1f020f0e36052a4c493a23e68fe2
lord-vector <r.anuj.agrawal@gmail.com>
2023-04-03 23:22:40 +0530

    Making small corrections in checkout and main

--- Pages/feature page/feature.apicall.js
+++ Pages/feature page/feature.apicall.js
@@ -17,9 +17,7 @@ const showItems = (items) => {
   str = '<ul class="grid">';
   items.forEach((element) => {
     str += `<div class="card" style="width: 18rem;">
-      <img src="${
-        element.images[0]
-      }" class="card-img-top imageShadow" alt="..." />
+      <img src="${element.images[0]}" class="card-img-top imageShadow" alt="..." />
       <div class="card-body">
         <h5 class="card-title">${element.title}</h5>
         <p class="card-text">
@@ -43,65 +41,67 @@ const showItems = (items) => {
 
 getText(queryString);
 
-
 // checkout section
 
-let arr = []
+let arr = [];
 
 const add = (btn) => {
-  arr.push(btn.name)
-  document.getElementById("noOfItems").innerHTML = arr.length
-  cart()
-}
-let total = 0
+  arr.push(btn.name);
+  document.getElementById("noOfItems").innerHTML = arr.length;
+  cart();
+};
+let total = 0;
 
 const cart = () => {
-  console.log(queryString)
+  console.log(queryString);
   fetch(`https://dummyjson.com/products/`)
     .then((res) => {
       // console.log(res);
       res.json().then((data) => {
         // console.log(data.products);
-        temp = data.products
+        temp = data.products;
         // console.log(temp[0].title)
-        let x = ""
-        var item = document.getElementById("cart")
+        let x = "";
+        var item = document.getElementById("cart");
         // item.style.backgroundColor ="red"
-        let qty = document.getElementsByClassName("qty")
-        console.log(qty.value)
-        let i = parseInt(arr[arr.length-1])-1
-        let finalPrice = Math.round((temp[i].price)-((temp[i].price)*(temp[i].discountPercentage)/100))
-        
-        total += finalPrice
-          
-          x += `
+        let qty = document.getElementsByClassName("qty");
+        console.log(qty.value);
+        let i = parseInt(arr[arr.length - 1]) - 1;
+        let finalPrice = Math.round(
+          temp[i].price - (temp[i].price * temp[i].discountPercentage) / 100
+        );
+
+        total += finalPrice;
+
+        x += `
           <div class = "cart-items">
             <img src="${temp[i].images[0]}"/>
             <h5>${temp[i].title}</h5>
             <div class="pricing">
               <h5 class="discountedPrice">$${finalPrice}</h5>
-              <p class="price">$${(temp[i].price)}</p>
+              <p class="price">$${temp[i].price}</p>
             </div>
           </div>
-          <hr>`
-          document.getElementById("checkoutPrice").innerHTML = total
+          <hr>`;
+        document.getElementById("checkoutPrice").innerHTML = total;
         const frag2 = document.createRange().createContextualFragment(x);
         item.appendChild(frag2);
       });
     })
-    .catch((err) => console.log(err));   
-}
+    .catch((err) => console.log(err));
+};
 
 const openCheckout = () => {
-  document.getElementById("cart").style.right = "0"
-  // document.body.style.backdropFilter = 
-  document.getElementById("cart").style.boxShadow = "5px 5px 1500px black"
-}
+  document.getElementById("cart").style.right = "0";
+  // document.body.style.backdropFilter =
+  document.getElementById("cart").style.boxShadow = "5px 5px 1500px black";
+};
 const closeCart = () => {
-  document.getElementById("cart").style.right = "-500px"
-  document.getElementById("cart").style.boxShadow = "none"
-}
+  document.getElementById("cart").style.right = "-600px";
+  document.getElementById("cart").style.boxShadow = "none";
+};
 
 const hooray = () => {
-  document.getElementsByClassName("cart-items")[0].innerHTML = "Order Successfully placed!<br>Thank you for shopping with us :)"
-}
+  document.getElementsByClassName("cart-items")[0].innerHTML =
+    "Order Successfully placed!<br>Thank you for shopping with us :)";
+};
