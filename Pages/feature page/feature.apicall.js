d6ed9461d269b6bccc7d2f94e4cc6b081feb821d
vishu pyasi <vpyasi11@gmail.com>
2023-04-03 19:48:20 +0530

    added checkout feature, modified home page navbar etc

--- Pages/feature page/feature.apicall.js
+++ Pages/feature page/feature.apicall.js
@@ -26,11 +26,9 @@ const showItems = (items) => {
           ${element.description}
         </p>
         <div class="actionButtons">
-            <div href="#" class="btn btn-primary ${(onclick = () => {
-              console.log(element.title);
-            })}">
+            <button href="#" class="btn btn-primary" onclick="add(this)" name="${element.id}">
             Add to Cart
-            </div>
+            </button>
             <div href="#" class="btn btn-primary">
             Favourites
             </div>
@@ -44,3 +42,66 @@ const showItems = (items) => {
 };
 
 getText(queryString);
+
+
+// checkout section
+
+let arr = []
+
+const add = (btn) => {
+  arr.push(btn.name)
+  document.getElementById("noOfItems").innerHTML = arr.length
+  cart()
+}
+let total = 0
+
+const cart = () => {
+  console.log(queryString)
+  fetch(`https://dummyjson.com/products/`)
+    .then((res) => {
+      // console.log(res);
+      res.json().then((data) => {
+        // console.log(data.products);
+        temp = data.products
+        // console.log(temp[0].title)
+        let x = ""
+        var item = document.getElementById("cart")
+        // item.style.backgroundColor ="red"
+        let qty = document.getElementsByClassName("qty")
+        console.log(qty.value)
+        let i = parseInt(arr[arr.length-1])-1
+        let finalPrice = Math.round((temp[i].price)-((temp[i].price)*(temp[i].discountPercentage)/100))
+        
+        total += finalPrice
+          
+          x += `
+          <div class = "cart-items">
+            <img src="${temp[i].images[0]}"/>
+            <h5>${temp[i].title}</h5>
+            <div class="pricing">
+              <h5 class="discountedPrice">$${finalPrice}</h5>
+              <p class="price">$${(temp[i].price)}</p>
+            </div>
+          </div>
+          <hr>`
+          document.getElementById("checkoutPrice").innerHTML = total
+        const frag2 = document.createRange().createContextualFragment(x);
+        item.appendChild(frag2);
+      });
+    })
+    .catch((err) => console.log(err));   
+}
+
+const openCheckout = () => {
+  document.getElementById("cart").style.right = "0"
+  // document.body.style.backdropFilter = 
+  document.getElementById("cart").style.boxShadow = "5px 5px 1500px black"
+}
+const closeCart = () => {
+  document.getElementById("cart").style.right = "-500px"
+  document.getElementById("cart").style.boxShadow = "none"
+}
+
+const hooray = () => {
+  document.getElementsByClassName("cart-items")[0].innerHTML = "Order Successfully placed!<br>Thank you for shopping with us :)"
+}
